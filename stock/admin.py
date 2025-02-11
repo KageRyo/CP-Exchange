02b3157e1a518ff473200a7e624446db406425b1
KageRyo <k6.developer@gmail.com>
2025-02-11 11:25:50 +0800

    feat: migrations

--- stock/admin.py
+++ stock/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Stock
 
 class StockAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'buy_price', 'sell_price', 'total_shares', 'is_active', 'updated_at', 'created_at')
+    list_display = ('id' ,'name', 'code', 'buy_price', 'sell_price', 'total_shares', 'is_active', 'updated_at', 'created_at')
 
 admin.site.register(Stock, StockAdmin)
