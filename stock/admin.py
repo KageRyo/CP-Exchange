3afd3953bdd91a20be633e5d7971d7cdf90a04c0
KageRyo <k6.developer@gmail.com>
2025-02-11 11:22:41 +0800

    feat: setup stock models & admin

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Stock
 
-# Register your models here.
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'buy_price', 'sell_price', 'total_shares', 'is_active', 'updated_at', 'created_at')
+
+admin.site.register(Stock, StockAdmin)
--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.IntegerField(unique=True)
+    buy_price = models.FloatField(default=0)
+    sell_price = models.FloatField(default=0)
+    total_shares = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
