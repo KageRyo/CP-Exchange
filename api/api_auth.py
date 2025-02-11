0da74037c21de360b7f2aa4df14dc23d2706170f
KageRyo <k6.developer@gmail.com>
2025-02-11 11:40:54 +0800

    feat: add TODO

---
+++ api/api_auth.py
@@ -0,0 +1,21 @@
+# guest
+# TODO: 註冊
+# TODO: 登入
+# TODO: Discord 登入
+
+# normal
+# TODO: Refresh
+# TODO: Verify Token
+# TODO: 登出
+# TODO: 修改密碼
+# TODO: 重設密碼
+# TODO: 更新個人資料
+# TODO: 取得個人資料
+
+# admin
+# TODO: 取得所有使用者資料
+# TODO: 建立使用者
+# TODO: 修改使用者
+# TODO: 刪除使用者
+# TODO: 重設使用者密碼
+# TODO: 更新使用者權限
---
+++ api/api_stock.py
@@ -0,0 +1,15 @@
+# normal
+# TODO: 取得證券資訊列表
+# TODO: 取得單一證券資訊
+# TODO: 取得持有的證券資訊
+# TODO: 買入證券
+# TODO: 賣出證券
+# TODO: 取得個人交易紀錄
+
+# admin
+# TODO: 建立新的證券資訊
+# TODO: 修改證券資訊
+# TODO: 開放交易證券
+# TODO: 停止交易證券
+# TODO: 刪除證券資訊
+# TODO: 取得所有交易紀錄
